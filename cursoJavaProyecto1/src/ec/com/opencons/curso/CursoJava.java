7654fe8f1bffdd34200c8efb2ef665b0ff6d6281
Axel <da266077@gmail.com>
2024-08-29 20:11:08 -0500

    Primer codigo fuente

--- cursoJavaProyecto1/src/ec/com/opencons/curso/CursoJava.java
+++ cursoJavaProyecto1/src/ec/com/opencons/curso/CursoJava.java
@@ -3,6 +3,9 @@
  */
 package ec.com.opencons.curso;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  */
@@ -14,7 +17,8 @@ public class CursoJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mindo");
-
+		List a=new ArrayList(); 
+        
 	}
 
 }
---
+++ cursoJavaProyecto1/src/ec/com/opencons/nota/NotaCurso.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package ec.com.opencons.nota;
+
+/**
+ * 
+ */
+public class NotaCurso {
+
+	/**
+	 * @param args
+	 */
+	/* mi tio dice que es el principal metodo 
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+//
